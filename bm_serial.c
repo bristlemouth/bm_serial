2c9869f49b945e3d8024b1ba68d3b8da8dbf6265
Russell DeGuzman <russell.deguzman@sofarocean.com>
2023-11-03 16:52:10 -0700

    pr feedback

--- bm_serial.c
+++ bm_serial.c
@@ -341,11 +341,12 @@ bm_serial_error_e bm_serial_send_network_info(
     memcpy(&network_info->config_crc, config_crc,
            sizeof(bm_common_config_crc_t));
     memcpy(&network_info->fw_info, fw_info, sizeof(bm_common_fw_version_t));
-    network_info->node_list.num_nodes = num_nodes;
-    memcpy(&network_info->node_list.list, node_id_list,
+    network_info->num_nodes = num_nodes;
+    memcpy(&network_info->node_list_and_cbor_config_map, node_id_list,
            sizeof(uint64_t) * num_nodes);
-    network_info->config_map.map_size_bytes = config_map_size;
-    memcpy(&network_info->config_map.map, cbor_config_map, config_map_size);
+    network_info->map_size_bytes = config_map_size;
+    size_t end_of_node_list = sizeof(uint64_t) * num_nodes;
+    memcpy(&network_info->node_list_and_cbor_config_map[end_of_node_list], cbor_config_map, config_map_size);
 
     packet->crc16 = bm_serial_crc16_ccitt(0, (uint8_t *)packet, message_len);
 
